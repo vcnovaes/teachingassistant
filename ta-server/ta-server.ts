78aa9b4ab15554b90c478451c2bbdf8771461bcd
vcnovaes <vcn2@cin.ufpe.br>
2023-03-26 19:26:50 -0300

    rearrange

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -1,15 +1,26 @@
-import express = require('express');
+import express = require("express");
 
 var taserver = express();
 
-var alunos = [{nome:'Paulo',cpf:'683',email:'phmb@cin.br',metas:{'requisitos':'MA'}},{nome:'Mariana',cpf:'456',email:'@mcb@cin.br',metas:{'requisitos':'MPA'}}];
+var alunos = [
+  {
+    nome: "Paulo",
+    cpf: "683",
+    email: "phmb@cin.br",
+    metas: { requisitos: "MA" },
+  },
+  {
+    nome: "Mariana",
+    cpf: "456",
+    email: "@mcb@cin.br",
+    metas: { requisitos: "MPA" },
+  },
+];
 
-taserver.get('/', function (req, res) {
+taserver.get("/", function (req, res) {
   res.send(alunos);
-})
+});
 
 taserver.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-})
-
-
+  console.log("Example app listening on port 3000!");
+});
